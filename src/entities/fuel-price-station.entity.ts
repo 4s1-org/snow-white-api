f309a27075c53b75630421bb519582c913810ed7
Steffen <gitlab@4s1.de>
2020-06-11 19:34:39 +0200

    add existing entities

---
+++ src/entities/fuel-price-station.entity.ts
@@ -0,0 +1,25 @@
+import { Entity, Column, PrimaryGeneratedColumn } from "typeorm"
+
+@Entity("FuelPriceStations")
+export class FuelPriceStationEntity {
+  @PrimaryGeneratedColumn("uuid")
+  public id: string
+
+  @Column({ length: 255 })
+  public name: string
+
+  @Column({ length: 255 })
+  public nameOrigin: string
+
+  @Column("int")
+  public latitude: number
+
+  @Column("int")
+  public longitude: number
+
+  @Column("int", { nullable: true })
+  public sortNo: number
+
+  @Column({ type: "uuid", length: 36 })
+  public remoteId: string
+}
