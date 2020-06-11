218d0047e4784364c8f263eff020efa0c817cea4
Steffen <steffen@4s1.de>
2020-06-11 19:34:39 +0200

    add existing entities

---
+++ src/entities/timetable-settings.entity.ts
@@ -0,0 +1,53 @@
+import { Entity, Column, PrimaryGeneratedColumn, ManyToOne } from "typeorm"
+import { TimetableStationEntity } from "./timetable-station.entity"
+
+@Entity("TimetableSettings")
+export class TimetableSettingsEntity {
+  @PrimaryGeneratedColumn("uuid")
+  public id: string
+
+  @Column()
+  public isActive: boolean
+
+  @Column({ length: 255 })
+  public apiKey: string
+
+  @ManyToOne(/* istanbul ignore next */ () => TimetableStationEntity, {
+    nullable: true,
+    onDelete: "SET NULL",
+  })
+  public timetableStationFrom: TimetableStationEntity
+
+  @ManyToOne(/* istanbul ignore next */ () => TimetableStationEntity, {
+    nullable: true,
+    onDelete: "SET NULL",
+  })
+  public timetableStationTo: TimetableStationEntity
+
+  @Column("int")
+  public maxChanges: number
+
+  @Column()
+  public showICE: boolean
+
+  @Column()
+  public showIC: boolean
+
+  @Column()
+  public showBus: boolean
+
+  @Column()
+  public showTram: boolean
+
+  @Column()
+  public showSBahn: boolean
+
+  @Column()
+  public showRE: boolean
+
+  @Column()
+  public showRB: boolean
+
+  @Column()
+  public showUBahn: boolean
+}
---
+++ src/entities/traffic-settings.entity.ts
@@ -0,0 +1,26 @@
+import { Entity, Column, PrimaryGeneratedColumn, ManyToOne } from "typeorm"
+import { CommonLocationEntity } from "./common-location.entity"
+
+@Entity("TrafficSettings")
+export class TrafficSettingsEntity {
+  @PrimaryGeneratedColumn("uuid")
+  public id: string
+
+  @Column()
+  public isActive: boolean
+
+  @Column({ length: 255 })
+  public apiKey: string
+
+  @ManyToOne(/* istanbul ignore next */ () => CommonLocationEntity, {
+    nullable: true,
+    onDelete: "SET NULL",
+  })
+  public commonLocationFrom: CommonLocationEntity
+
+  @ManyToOne(/* istanbul ignore next */ () => CommonLocationEntity, {
+    nullable: true,
+    onDelete: "SET NULL",
+  })
+  public commonLocationTo: CommonLocationEntity
+}
