5fee603a10fbb33308d326cfcd10a0554a86c251
Steffen <steffen@4s1.de>
2021-05-01 19:01:08 +0200

    Anwendungen laufen wieder so halb

--- src/smartmirror/admin/date/date.module.ts
+++ src/smartmirror/admin/date/date.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common'
 import { DateSettingDbService } from '../../../database/date-setting-db.service'
+import { PrismaService } from '../../../database/prisma.service'
 import { DateController } from './date.controller'
 import { DateSettingsService } from './settings/date-settings.service'
 
 @Module({
   controllers: [DateController],
   imports: [],
-  providers: [DateSettingsService, DateSettingDbService],
+  providers: [DateSettingsService, DateSettingDbService, PrismaService],
 })
 export class DateModule {}
