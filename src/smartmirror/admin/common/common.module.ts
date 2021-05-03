fdd41802228c583d948b1b19ddf0978d698e2bf3
Steffen <yellowgarbagebag@4s1.de>
2021-05-03 17:45:25 +0200

    CommonLocationsService

--- src/smartmirror/admin/common/common.module.ts
+++ src/smartmirror/admin/common/common.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common'
 import { CommonController } from './common.controller'
-import { CommonLocationsService } from './locations/common-locations.service'
 import { RemoteApiCallModule } from '../../../remote-api-call/remote-api-call.module'
 import { CommonSettingDbService } from '../../../database/common-setting-db.service'
 import { CommonLocationDbService } from '../../../database/common-location-db.service'
@@ -9,6 +8,6 @@ import { PrismaService } from '../../../database/prisma.service'
 @Module({
   controllers: [CommonController],
   imports: [RemoteApiCallModule],
-  providers: [CommonLocationsService, CommonSettingDbService, CommonLocationDbService, PrismaService],
+  providers: [CommonSettingDbService, CommonLocationDbService, PrismaService],
 })
 export class CommonModule {}
