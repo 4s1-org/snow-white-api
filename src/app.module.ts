3f98e84a4356964ed45ffa475c3466a6e0cbeb72
Steffen <yellowgarbagebag@4s1.de>
2021-07-02 23:14:48 +0200

    mqtt

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,8 @@ import { RemoteApiCallModule } from './remote-api-call/remote-api-call.module'
 import { SmartmirrorModule } from './smartmirror/smartmirror.module'
 import { PrismaService } from './database/prisma.service'
 import { ConfigService } from './config/config.service'
+import { StockController } from './stock/stock.controller'
+import { ClientsModule, Transport } from '@nestjs/microservices'
 
 import * as dotenv from 'dotenv'
 dotenv.config()
@@ -14,8 +16,26 @@ const logger = new Logger('App Module')
 logger.log('Lets go')
 
 @Module({
-  controllers: [TimetrackingController],
-  imports: [HttpModule, TimetrackingModule, RemoteApiCallModule, SmartmirrorModule],
+  controllers: [TimetrackingController, StockController],
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'MATH_SERVICE',
+        transport: Transport.MQTT,
+        options: {
+          url: 'mqtts://192.168.0.1',
+          port: 8883,
+          username: 'username',
+          password: 'password',
+          rejectUnauthorized: false,
+        },
+      },
+    ]),
+    HttpModule,
+    TimetrackingModule,
+    RemoteApiCallModule,
+    SmartmirrorModule,
+  ],
   providers: [TimetrackingService, PrismaService, ConfigService],
 })
 export class AppModule {}
--- src/stock/stock.controller.ts
+++ src/stock/stock.controller.ts
@@ -1,12 +1,20 @@
-import { Controller, Get, Param } from '@nestjs/common'
+import { Controller, Get, Param, Inject } from '@nestjs/common'
+import { ClientProxy } from '@nestjs/microservices'
 
 @Controller('stock')
 export class StockController {
-  @Get('/:id')
-  public loadSingleStation(@Param('id') id: string): any {
+  constructor(@Inject('MATH_SERVICE') private client: ClientProxy) {}
+
+  async onApplicationBootstrap(): Promise<void> {
+    await this.client.connect()
+  }
+
+  @Get('/:isin')
+  public loadSingleStation(@Param('isin') isin: string): any {
     const res = {
-      foo: id,
+      foo: isin,
     }
+    this.client.emit<void, any>(`snowwhite/event/stock/${isin}`, Date.now)
     return res
   }
 }
