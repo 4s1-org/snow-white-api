d7aa8c77eb77301b8f3dc78d436adb867b17d5f8
Steffen <steffen@4s1.de>
2021-07-04 10:58:28 +0200

    Linter

--- src/stock/stock.controller.spec.ts
+++ src/stock/stock.controller.spec.ts
@@ -1,18 +1,18 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { StockController } from './stock.controller';
+import { Test, TestingModule } from '@nestjs/testing'
+import { StockController } from './stock.controller'
 
 describe('StockController', () => {
-  let controller: StockController;
+  let controller: StockController
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [StockController],
-    }).compile();
+    }).compile()
 
-    controller = module.get<StockController>(StockController);
-  });
+    controller = module.get<StockController>(StockController)
+  })
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
-});
+    expect(controller).toBeDefined()
+  })
+})
