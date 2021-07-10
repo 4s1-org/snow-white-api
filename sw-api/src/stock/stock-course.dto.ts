dc1089a31cf819ea0d935a1a7805a65b6de420d8
Steffen <yellowgarbagebag@4s1.de>
2021-07-10 19:43:58 +0200

    Laden von Stocks aus MQTT

---
+++ sw-api/src/stock/stock-course.dto.ts
@@ -0,0 +1,10 @@
+export class StockCourseDto {
+  public timestamp!: number
+  public dateTime!: string
+  public last!: number
+  public high!: number
+  public low!: number
+  public lastUpdate!: number
+  public isin!: string
+  public name!: string
+}
