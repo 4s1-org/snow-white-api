19c973763212fc1310cef10d4bb212b4caf94fa5
Steffen <steffen@4s1.de>
2021-10-31 19:24:33 +0100

    feat: sqlite database can be used

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,9 @@ import { RemoteApiCallModule } from './remote-api-call/remote-api-call.module.js
 import { SmartmirrorModule } from './smartmirror/smartmirror.module.js'
 import { ConfigService } from './config/config.service.js'
 import { StockController } from './stock/stock.controller.js'
-
+import * as entities from './entities/index.js'
 import * as dotenv from 'dotenv'
-import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm'
-import { ConfigModule } from './config/config.module.js'
+import { TypeOrmModule } from '@nestjs/typeorm'
 dotenv.config()
 
 const logger = new Logger('App Module')
@@ -18,40 +17,12 @@ logger.log('Lets go')
 @Module({
   controllers: [TimetrackingController, StockController],
   imports: [
-    // ToDo: Database access
-    //   TypeOrmModule.forRootAsync({
-    //   imports: [ConfigModule],
-    //   inject: [ConfigService],
-    //   useFactory: (config: ConfigService) => {
-    //     const host: string = config.get('MYSQL_HOST')
-    //     const port: number = config.get('MYSQL_PORT')
-    //     const database: string = config.get('MYSQL_DATABASE')
-    //     const username: string = config.get('MYSQL_USER')
-    //     const password: string = config.get('MYSQL_PASSWORD')
-    //     const ssl: boolean = config.get('MYSQL_SSL') !== 'false'
-    //     const logging: boolean = config.get('MYSQL_LOGGING') === 'true'
-
-    //     logger.log('MYSQL_HOST: ' + host)
-    //     logger.log('MYSQL_PORT: ' + port)
-    //     logger.log('MYSQL_DATABASE: ' + database)
-    //     logger.log('MYSQL_USER: ' + username)
-    //     logger.log('MYSQL_SSL: ' + ssl)
-    //     logger.log('MYSQL_LOGGING: ' + logging)
-
-    //     return {
-    //       database,
-    //       entities: [__dirname + '/**/*.entity{.ts,.js}'],
-    //       host,
-    //       logging,
-    //       password,
-    //       port,
-    //       ssl,
-    //       synchronize: true,
-    //       type: 'mysql',
-    //       username,
-    //     } as TypeOrmModuleOptions
-    //   },
-    // }),
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'data/db.sqlite',
+      entities: [...Object.values(entities)],
+      synchronize: true,
+    }),
     HttpModule,
     TimetrackingModule,
     RemoteApiCallModule,
